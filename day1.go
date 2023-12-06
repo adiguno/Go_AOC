6217e50b7785610d99a2b1bbc4fcb0a5101f2669
Dian <guodian44@gmail.com>
2023-12-06 19:03:58 +0800

    remove testing code comments

--- day1.go
+++ day1.go
@@ -6,19 +6,10 @@ import (
 	"os"
 	"strconv"
 	"unicode"
-	// "io/ioutil"
-	// "rsc.io/quote"
 )
 
 func main() {
 	// mainStrings()
-
-	// read entire file as a string
-	// fileBytes, err := ioutil.ReadFile("day1_input")
-	// if err != nil {
-	// 	fmt.Print(err)
-	// }
-	// fileString := string(fileBytes)
 
 	// read file
 	file, err := os.Open("day1_input")
@@ -39,7 +30,6 @@ func main() {
 
 	var total int
 	for index, line := range lines {
-		// fmt.Println(i, line)
 		lineSum := getSum(getDigits(line))
 		total += lineSum
 		fmt.Printf("index %d: ", index)
@@ -49,19 +39,6 @@ func main() {
 
 	}
 
-	// testing
-	// total += getSum(getDigits("1abc2"))
-	// a := getSum(getDigits("1abc2"))
-	// // a := getSum(getDigitsForRangeLoop("1labc2"))
-	// if a == 12 {
-	// 	fmt.Println("sum working: yes")
-	// } else if a == 540 {
-	// 	fmt.Println("bad")
-	// }
-	// // fmt.Println("First Integer:", 12)
-
-	// fmt.Println(string(rune((total))))
-	// fmt.Printf("Type of total: %T\n", total)
 	fmt.Println("total", total)
 
 }
@@ -75,11 +52,6 @@ func getSum(num1 int, num2 int) int {
 // single number => same number repeated
 // 2+ numbers
 func getDigits(text string) (int, int) {
-	// fmt.Println(text)
-	// for _, char := range text {
-	// 	// fmt.Println(char) // outputs unicode e.g. 116, 108
-	// 	fmt.Printf("%c\n", char)
-	// }
 	var first, last int
 	var firstFound, lastFound bool
 	i := 0
@@ -88,90 +60,19 @@ func getDigits(text string) (int, int) {
 		char := text[i] //byte, rune?
 		if unicode.IsDigit(rune(char)) {
 			if !firstFound {
-				// first = int(char)
-				// firstRune := rune(char)
-				// first = int(firstRune)
 				first, _ = strconv.Atoi(string(char))
 				last = first
 
-				// fmt.Prsntln("char", char)
-				// fmt.Println("firstRune", firstRune)
-				// fmt.Println("firstRune int", int(firstRune))
-				// fmt.Println("firstRune int string", string(int(firstRune))) // 1 expected
-				// fmt.Println(string(char))
-				// fmt.Printf("first: %d\n", first)
-				// fmt.Println("firstRune int string", first) // 1 expected
 				firstFound = true
 				lastFound = true
 			} else if lastFound {
-				// else if firstFound && !lastFound {
-				// 	// last = int(rune(char))
-				// 	last, _ = strconv.Atoi(string(char))
 
-				// 	lastFound = true
-				// }
-				// last = int(rune(char))
 				last, _ = strconv.Atoi(string(char))
 				lastFound = true
 			}
 		}
 		i++
 	}
-
-	// fmt.Println("first: " + string(rune(first)))
-	// fmt.Println("last: " + string(rune(last)))
-	// first = 1
-	// fmt.Println("first: ", first)
-	// fmt.Printf("printf first: %d\n", first)
-	// fmt.Println("last: ", last)
-	// fmt.Printf("type of last: %T\n", last)
-	// if last == 2 {
-	// 	fmt.Println("okayyyy ")
-
-	// }
-	return first, last
-}
-
-func getDigitsForRangeLoop(text string) (int, int) {
-	// fmt.Println(text)
-	// for _, char := range text {
-	// 	// fmt.Println(char) // outputs unicode e.g. 116, 108
-	// 	fmt.Printf("%c\n", char)
-	// }
-	var first, last int
-	var firstFound, lastFound bool
-	for _, rune1 := range text {
-
-		if unicode.IsDigit(rune1) {
-			if !firstFound {
-				// first, _ = strconv.Atoi(rune1)
-				// fmt.Println("firstRune int string", string(int(firstRune))) // 1 expected
-				// fmt.Println(string(char))
-				// fmt.Printf("first: %d\n", first)
-				firstFound = true
-			} else if firstFound && !lastFound {
-				last = int(rune1)
-				lastFound = true
-			} else if lastFound {
-				last = int(rune1)
-				lastFound = true
-			}
-		}
-
-	}
-
-	// fmt.Println("first: " + string(rune(first)))
-	// fmt.Println("last: " + string(rune(last)))
-	// first = 1
-	fmt.Println("first: ", first)
-	fmt.Println("last: ", last)
-	fmt.Printf("printf first: %d\n", first)
-	fmt.Printf("printf last: %d\n", last)
-	fmt.Printf("type of last: %T\n", last)
-	if last == 2 {
-		fmt.Println("okayyyy ")
-	}
-	// result, err := strconv.Atoi(first)
 
 	return first, last
 }
